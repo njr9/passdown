75fa23061c6a9de096efce84af3dbd2707e41db2
Nate Reyes <njr9@students.uwf.edu>
2017-11-27 19:46:02 -0600

    changed project base

---
+++ app/src/main/java/passdown/com/forsale/SearchFragment.java
@@ -0,0 +1,20 @@
+package passdown.com.forsale;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+    private static final String TAG = "SearchFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return view;
+    }
+}
